b406ebe25f901180350c696c400c49f4789fcb76
ergodiclife <ergodiclife@pm.me>
2020-01-12 01:05:28 +0000

    Add scala stuff.

--- python/code/663/series.py
+++ python/code/663/series.py
@@ -1,3 +1,16 @@
 #!C:/Users/Chris/Anaconda3/python.exe
 #
 # http://people.duke.edu/~ccc14/sta-663-2019/index.html
+import pandas as pd
+
+df = pd.read_csv('https://bit.ly/2RIw7Ig', index_col=0)
+
+### Basic operations
+df.columns
+df.index
+df.shape
+df.dtypes
+df.head(5)
+df.tail(5)
+df.sample(5)
+
